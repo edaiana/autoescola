52929d6bc3123bd1f3c954d384723caf40d18872
Edaiana <edaiana.oliveira@portoseguro.com.br>
2015-11-07 20:26:37 -0200

    Alterações do trabalho

--- login.php
+++ login.php
@@ -1,3 +1,11 @@
-<?php
-echo("Hello World");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title><?php echo("Auto-escola Dirige Fácil")?></title>
+</head>
+<body>
+
+<?php echo("sistema de auto-escola Dirige Fácil")?>
+</body>
+</html>
